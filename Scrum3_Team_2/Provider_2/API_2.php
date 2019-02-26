c0c8030077721f291ab81a428d3e15e9255c1845
MaxMcElhone <Maxwell.McElhone@gmail.com>
2019-02-25 19:41:40 -0600

    Finished the api and added a page to get all the records from all the tables.

--- Scrum3_Team_2/Provider_2/API_2.php
+++ Scrum3_Team_2/Provider_2/API_2.php
@@ -9,26 +9,113 @@ $tableName = $_POST['tableName'];
 $method = $_POST['method'];
 
 $db = new ScrumDB();
+$result;
 
-if($method == 'getOne'){
+if(strcmp($method, 'getOne') == 0){
   $ID = $_POST('ID');
-  $data = $db->getOneRecord($tableName, $ID);
-  
+  $result = $db->getOneRecord($tableName, $ID);
+
 }
-elseif ($method == 'getAll') {
-  // code...
+elseif (strcmp($method, 'getAll') == 0) {
+  $result = $db->getRecords($tableName);
+
 }
-elseif ($method == 'update') {
-  // code...
+elseif (strcmp($method, 'update') == 0) {
+    $ID = $_POST('ID');
+
+     $params = array();
+     if (strcmp($tableName, "customers") == 0) {
+         $params['firstName'] = $_POST["firstName"];
+         $params['lastName'] = $_POST["lastName"];
+         $params['email'] = $_POST['email'];
+         $params['phone'] = $_POST['phone'];
+     }
+     else if (strcmp($tableName, "orders") == 0) {
+         $params['ShippingAddress'] = $_POST["shippingAddress"];
+         $params['orderDate'] = $_POST["orderDate"];
+         $params['ExpectedArivalDate'] = $_POST['expectedArivalDate'];
+         $params['Price'] = $_POST['price'];
+     }
+     else if (strcmp($tableName, "products") == 0) {
+         $params['name'] = $_POST["name"];
+         $params['color'] = $_POST["color"];
+         $params['description'] = $_POST['description'];
+         $params['price'] = $_POST['price'];
+     }
+
+     $db->updateRecord($tableName, $ID, $params);
+     $result->getOneRecord($tableName, $ID);
 }
-elseif ($method == 'insert') {
-  // code...
+elseif (strcmp($method, 'insert') == 0) {
+    $params = array();
+    if (strcmp($tableName, "customers") == 0) {
+        $params['firstName'] = $_POST["firstName"];
+        $params['lastName'] = $_POST["lastName"];
+        $params['email'] = $_POST['email'];
+        $params['phone'] = $_POST['phone'];
+    }
+    else if (strcmp($tableName, "orders") == 0) {
+        $params['ShippingAddress'] = $_POST["shippingAddress"];
+        $params['orderDate'] = $_POST["orderDate"];
+        $params['ExpectedArivalDate'] = $_POST['expectedArivalDate'];
+        $params['Price'] = $_POST['price'];
+    }
+    else if (strcmp($tableName, "products") == 0) {
+        $params['name'] = $_POST["name"];
+        $params['color'] = $_POST["color"];
+        $params['description'] = $_POST['description'];
+        $params['price'] = $_POST['price'];
+    }
+
+    $result = $db->insertRecord($tableName, $params);
 }
-elseif ($method == 'update') {
-  // code...
+elseif (strcmp($method, 'delete') == 0) {
+    $ID = $_POST('ID');
+    $db->insertRecord($tableName, $ID);
 }
-elseif ($method == 'delete') {
-  // code...
+
+$data = array();
+// customers
+if ( strcmp($tableName, "customers") == 0){
+  while ($row = $result->fetch_assoc()){
+    $c = new Customer();
+    $c->setID($row['ID']);
+    $c->setFirstName($row['firstName']);
+    $c->setLastName($row['lastName']);
+    $c->setEmail($row['email']);
+    $c->setPhone($row['phone']);
+
+    $data[] = (array) $c;
+  }
 }
+
+// products
+else if ( strcmp($tableName, "products") == 0){
+  while ($row = $result->fetch_assoc()){
+    $p = new Product();
+    $p->setID($row['ID']);
+    $p->setName($row['name']);
+    $p->setColor($row['color']);
+    $p->setDescription($row['description']);
+    $p->setPrice($row['price']);
+
+    $data[] = (array) $p;
+  }
+}
+
+// orders
+else if ( strcmp($tableName, "orders") == 0){
+  while ($row = $result->fetch_assoc()){
+    $o = new Order();
+    $o->setID($row['ID']);
+    $o->setShippingAddress($row['ShippingAddress']);
+    $o->setOrderDate($row['orderDate']);
+    $o->setExpectedArivalDate($row['ExpectedArivalDate']);
+    $o->setPrice($row['Price']);
+
+    $data[] = (array) $o;
+  }
+}
+ print(json_encode($data));
 
  ?>
